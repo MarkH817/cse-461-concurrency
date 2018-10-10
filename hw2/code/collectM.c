88d4f9dcb6c87a7e230ae4209f08cc450ea7da4c
Mark Hernandez <mark@lion-byte.com>
2018-10-10 12:08:08 -0500

    Create collectM.c file

---
+++ hw2/code/collectM.c
@@ -0,0 +1,85 @@
+/**
+ * Team Members:
+ *
+ * - Mark Hernandez
+ * - Adrian Ariffin
+ * - Greg Nail
+ *
+ * ---
+ *
+ * Absolute Speed-Up:
+ *
+ * - [Insert result here]
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "helper/helper.h"
+
+Node *start = NULL;
+
+void addData(int);
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    //char filename[50][128];
+    int numfile = 0;
+    char buffer[128];
+    int i = 0, dat;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: ./collectS filename \n");
+        exit(0);
+    }
+
+    if (!(fp = fopen(argv[1], "r")))
+    {
+        return 0;
+    }
+
+    while (fscanf(fp, "%s\n", buffer) != EOF)
+    {
+        numfile++;
+    }
+
+    char filename[numfile][128];
+    fseek(fp, 0, SEEK_SET);
+
+    while (fscanf(fp, "%s\n", filename[i]) != EOF)
+    {
+        i++;
+    }
+
+    fclose(fp);
+
+    for (i = 0; i < numfile; i++)
+    {
+        sprintf(buffer, "../data/%s", filename[i]);
+        if (!(fp = fopen(buffer, "r")))
+        {
+            return 0;
+        }
+
+        while (fscanf(fp, "%d\n", &dat) != EOF)
+        {
+            addData(dat);
+        }
+
+        fclose(fp);
+    }
+
+    printList(start);
+    destroyList(start);
+}
+
+void addData(int dat)
+{
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = dat;
+    newNode->next = start;
+    start = newNode;
+}
